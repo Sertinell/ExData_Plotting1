046e475c43e767e0934f7d369cf73bac2d80bc02
Sergio Martínez Tornell <sertinell@gmail.com>
2015-11-02 22:55:45 +0000

    Plot 4 updated

--- plot4.R
+++ plot4.R
@@ -2,9 +2,10 @@ source("readdata.R")
 energydata <- readdata()
 
 #define the grid
-par(mfrow=c(2,2))
 
 png("plot4.png", width=504, height = 504, units="px")
+par(mfrow=c(2,2))
+
 plot(energydata$datetime, energydata$Global_active_power, type = "lines", 
      xlab = "",
      ylab = "Global Active Powers")
@@ -16,6 +17,9 @@ lines(energydata$datetime, energydata$Sub_metering_1, col = "black")
 lines(energydata$datetime, energydata$Sub_metering_2, col = "red")
 lines(energydata$datetime, energydata$Sub_metering_3, col = "blue")
 legend("topright", legend = (names(energydata)[7:9]), 
-       col = c("black", "red", "blue"), lty = c(1,1), border = FALSE )
+       col = c("black", "red", "blue"), lty = c(1,1), border = FALSE)
+plot(energydata$datetime, energydata$Global_reactive_power, type = "lines", 
+     xlab = "datetime",
+     ylab = "Global_reactive_power")
 
 dev.off()
